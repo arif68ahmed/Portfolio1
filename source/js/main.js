4b3c44896451d40a6e25f40e58bc1c1bbe680bd1
arif68ahmed <arif68ahmed@gmail.com>
2020-05-30 03:39:37 +0600

    Add files via upload

---
+++ source/js/main.js
@@ -0,0 +1,24 @@
+
+ $(document).ready(function(){
+ $("ul").hide()
+
+
+  $(".btn").click(function(){
+  $(".menu-btn").show()  
+                        })               
+
+ $(".btn").click(function(){
+ $("ul").toggle()
+      })
+
+
+
+
+
+
+
+
+
+
+
+
